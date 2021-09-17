263c51a82ad5f04646a06ec83cdb196776ac532f
Neomonolith <90251446+Neomonolith@users.noreply.github.com>
2021-09-17 10:11:06 +0200

    Add project files.

---
+++ Peter-Erik/Bérek2020.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Peter_Erik
+{
+  class Program
+  {
+    static List<adatok> szamolasok = new List<adatok>();
+    static void Main(string[] args)
+    {
+      //néven mentés Elsofeladat();
+      Masodikfeladat();
+      Harmadikfeladat();
+      Negyedikfeladat();
+      Otodikfeladat();
+      Hatodikfeladat();
+      Hetedikfeladat();
+
+
+      Console.ReadLine();
+    }
+
+    private static void Hetedikfeladat()
+    {
+     
+    }
+
+    private static void Hatodikfeladat()
+    {
+     
+    }
+
+    private static void Otodikfeladat()
+    {
+      
+
+
+      string[] a;
+      Console.Write("5. feladat: Kérem egy részleg nevét: ");
+      a = Console.ReadLine().Split();
+      
+      foreach (var l in szamolasok)
+      {
+
+       
+      }
+    }
+
+    private static void Negyedikfeladat()
+    {
+
+      /*
+      double osztas = 0;
+      int osszeg = 0;
+      foreach (var l in szamolasok)
+      {
+        if (l.Belepes == 2020)
+        {
+          osszeg = l.Ber / osszeg;
+          //osszeg = osszeg + l.Ber;
+          //osztas = (double)osszeg + l.Ber / 1000;
+          Console.WriteLine($"4. feladat: Bérek átlaga:{osztas}");
+          
+        
+          //Math.round()
+         
+        }
+      }
+
+      */
+    }
+
+
+
+    private static void Harmadikfeladat()
+    {
+      Console.WriteLine($"3. Feladat: Dolgozók száma:{szamolasok.Count()}");
+    }
+
+   
+    
+
+  private static void Masodikfeladat()
+  {
+      Console.WriteLine("2. feladat: adatok beolvasása");
+      
+      StreamReader be = new StreamReader("berek2020.txt");
+      be.ReadLine();
+      while (!be.EndOfStream)
+        {
+        string[] a = be.ReadLine().Split(';');
+
+        szamolasok.Add(new adatok(a[0], a[1], a[2], int.Parse(a[3]), int.Parse(a[4]))
+          );
+
+
+
+      }
+        be.Close();
+      }
+  }
+}
---
+++ Peter-Erik/adatok.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peter_Erik
+{
+  class adatok
+  {
+
+    private string nev;
+
+    public string Nev
+    {
+      get { return nev; }
+      set { nev = value; }
+    }
+
+
+    private string neme;
+
+    public string Neme
+    {
+      get { return neme; }
+      set { neme = value; }
+    }
+
+
+    private string reszleg;
+
+    public string Reszleg
+    {
+      get { return reszleg; }
+      set { reszleg = value; }
+    }
+
+    private int belepes;
+
+    public int Belepes
+    {
+      get { return belepes; }
+      set { belepes = value; }
+    }
+
+    private int ber;
+
+    public int Ber
+    {
+      get { return ber; }
+      set { ber = value; }
+    }
+
+    public adatok(string nev, string neme, string reszleg, int belepes, int ber)
+    {
+      this.nev = nev;
+      this.neme = neme;
+      this.reszleg = reszleg;
+      this.belepes = belepes;
+      this.ber = ber;
+    }
+
+  }
+}
